4d797e46e156f9d1ffc2efeb30fd164856199e65
Prado <ppmatheus01@gmail.com>
2024-10-02 16:30:17 -0300

    Att banco PI
    Atualizando o banco de dados NoSql do Projeto Integrador

--- StrayCode/StrayCode.mongodb.js
+++ StrayCode/StrayCode.mongodb.js
@@ -102,133 +102,114 @@ db.cnae.insertMany([
         numeroCnae: "0113-0/00",
         descricao: "Cultivo de cana-de-açúcar",
         ramoEmpresa: [2],
-        tributacao: []
     },
     {
         id: 2,
         numeroCnae: "0131-8/00",
         descricao: "Cultivo de laranja",
         ramoEmpresa: [2],
-        tributacao: []
     },
     {
         id: 3,
         numeroCnae: "0210-1/01",
         descricao: "Cultivo de eucalipto",
         ramoEmpresa: [3],
-        tributacao: []
     },
     {
         id: 4,
         numeroCnae: "0220-9/04",
         descricao: "Coleta de látex em florestas nativas",
         ramoEmpresa: [3],
-        tributacao: []
     },
     {
         id: 5,
         numeroCnae: "0500-3/01",
         descricao: "Extração de carvão mineral",
         ramoEmpresa: [4],
-        tributacao: []
     },
     {
         id: 6,
         numeroCnae: "0710-3/01",
         descricao: "Extração de minério de ferro",
         ramoEmpresa: [5],
-        tributacao: []
     },
     {
         id: 7,
         numeroCnae: "0721-9/01",
         descricao: "Extração de minério de alumínio",
         ramoEmpresa: [5],
-        tributacao: []
     },
     {
         id: 8,
         numeroCnae: "3600-6/01",
         descricao: "Captação, tratamento e distribuição de água",
         ramoEmpresa: [6],
-        tributacao: []
     },
     {
         id: 9,
         numeroCnae: "3600-6/02",
         descricao: "Distribuição de água por caminhões",
         ramoEmpresa: [6],
-        tributacao: []
     },
     {
         id: 10,
         numeroCnae: "3811-4/00",
         descricao: "Coleta de resíduos não perigosos",
         ramoEmpresa: [7],
-        tributacao: []
     },
     {
         id: 11,
         numeroCnae: "3900-5/00",
         descricao: "Descontaminação e outros serviços de gestão de resíduos",
         ramoEmpresa: [7],
-        tributacao: []
     },
     {
         id: 12,
         numeroCnae: "4511-1/01",
         descricao: "Comércio a varejo de automóveis, camionetas e utilitários novos",
         ramoEmpresa: [8],
-        tributacao: []
     },
     {
         id: 13,
         numeroCnae: "4512-9/01",
         descricao: "Representantes comerciais e agentes do comércio de veículos automotores",
         ramoEmpresa: [8],
-        tributacao: []
     },
     {
         id: 14,
         numeroCnae: "4611-7/00",
         descricao: "Representantes comerciais e agentes do comércio de matérias-primas agrícolas e animais vivos",
         ramoEmpresa: [9],
-        tributacao: []
     },
     {
         id: 15,
         numeroCnae: "4621-4/00",
         descricao: "Comércio atacadista de café em grão",
         ramoEmpresa: [9],
-        tributacao: []
     },
     {
         id: 16,
         numeroCnae: "4912-4/01",
         descricao: "Transporte ferroviário de passageiros intermunicipal e interestadual",
         ramoEmpresa: [10],
-        tributacao: []
     },
     {
         id: 17,
         numeroCnae: "4923-0/01",
         descricao: "Serviço de táxi",
         ramoEmpresa: [10],
-        tributacao: []
     },
     {
         id: 18,
         numeroCnae: "5011-4/02",
         descricao: "Transporte marítimo de cabotagem - Passageiros",
         ramoEmpresa: [11],
-        tributacao: []
     },
     {
         id: 19,
         numeroCnae: "5011-4/01",
         descricao: "Transporte marítimo de cabotagem - Carga",
         ramoEmpresa: [11],
-        tributacao: []
     }    
 ])
 
@@ -278,7 +259,7 @@ db.usuario.insertMany([
 db.questionario.insertMany([
     {
         id: 1,
-        dtQuestionario: "2024-09-23",
+        dtQuestionario: new Date("2024-05-23"),
         cnae: [
                 {
                     id: 18,
@@ -293,11 +274,12 @@ db.questionario.insertMany([
                     ramoEmpresa: [11]
                 }
             ],
-        usuario: [1]
+        usuario: [1],
+        tributacao: []
     },
     {
         id: 2,
-        dtQuestionario: "2024-09-16",
+        dtQuestionario: new Date("2024-09-16"),
         cnae: [
                 {
                     id: 5,
@@ -306,11 +288,12 @@ db.questionario.insertMany([
                     ramoEmpresa: [4]
                 }
             ],
-        usuario: [2]
+        usuario: [2],
+        tributacao: []
     },
     {
         id: 3,
-        dtQuestionario: "2024-09-30",
+        dtQuestionario: new Date("2024-07-30"),
         cnae: [
                 {
                     id: 17,
@@ -319,11 +302,12 @@ db.questionario.insertMany([
                     ramoEmpresa: [10]
                 }
             ],
-        usuario: [3]
+        usuario: [3],
+        tributacao: [1]
     },
     {
         id: 4,
-        dtQuestionario: "2024-09-30",
+        dtQuestionario: new Date("2024-03-30"),
         cnae: [
                 {
                     id: 12,
@@ -338,11 +322,12 @@ db.questionario.insertMany([
                     ramoEmpresa: [8]
                 }
             ],
-        usuario: [4]
+        usuario: [4],
+        tributacao: []
     },
     {
         id: 5,
-        dtQuestionario: "2024-09-30",
+        dtQuestionario: new  Date("2024-10-15"),
         cnae: [
                 {
                     id: 2,
@@ -351,7 +336,8 @@ db.questionario.insertMany([
                     ramoEmpresa: [2]
                 }
             ],
-        usuario: [5]
+        usuario: [5],
+        tributacao: [1]
     }
 ])
 
@@ -359,37 +345,28 @@ db.tributacao.insertMany([
     {
         id: 1,
         tiposTributacao: "MEI",
-        descricao: "O regime do MEI foi criado em 2008 para reduzir os níveis de informalidade, proporcionando aos empreendedores meios mais acessíveis de regularização fiscal. Para tanto, o MEI se apresenta de forma menos burocrática, tendo regras diferenciadas para o pagamento de impostos — IRPJ e CLSS, por exemplo, não entram no cálculo tributário dos microempreendedores individuais. Ainda sobre o pagamento de impostos, os valores de contribuição também são diferenciados no MEI: os cadastrados pagam mensalmente um valor de até R$ 60,60 — de acordo com a atividade exercida. Para que possam se enquadrar neste regime tributário, os empresários devem ter um faturamento bruto anual de até R$ 81 mil e não possuir sócios. Atendendo a esses requisitos, eles podem contar com uma série de benefícios, inclusive previdenciários, como: aposentadoria por idade, licença-maternidade, auxílio-doença, entre outros."
+        descricao: "O regime do MEI foi criado em 2008 para reduzir os níveis de informalidade, proporcionando aos empreendedores meios mais acessíveis de regularização fiscal. Para tanto, o MEI se apresenta de forma menos burocrática, tendo regras diferenciadas para o pagamento de impostos — IRPJ e CLSS, por exemplo, não entram no cálculo tributário dos microempreendedores individuais. Ainda sobre o pagamento de impostos, os valores de contribuição também são diferenciados no MEI: os cadastrados pagam mensalmente um valor de até R$ 60,60 — de acordo com a atividade exercida. Para que possam se enquadrar neste regime tributário, os empresários devem ter um faturamento bruto anual de até R$ 81 mil e não possuir sócios. Atendendo a esses requisitos, eles podem contar com uma série de benefícios, inclusive previdenciários, como: aposentadoria por idade, licença-maternidade, auxílio-doença, entre outros.",
+        cnae: []
     },
     {
         id: 2,
         tiposTributacao: "Simples Nacional",
-        descricao: "Reúne em um único documento, chamado de DAS (Documento de Arrecadação do Simples Nacional), as 8 taxas que devem ser pagas; - Possui alíquotas menores que os demais regimes; - É menos burocrático; - Para se enquadrar no Simples, é preciso ser microempreendedor individual (MEI), microempresa (ME) ou empresa de pequeno porte (EPP) e cumprir os seguintes requisitos financeiros: 1 - O MEI não pode ter faturamento anual acima de R$ 81 mil; 2 - Para ME, o teto é de R$ 360 mil por ano; 3 - O limite anual para a EPP é R$ 4,8 milhões."
+        descricao: "Reúne em um único documento, chamado de DAS (Documento de Arrecadação do Simples Nacional), as 8 taxas que devem ser pagas; - Possui alíquotas menores que os demais regimes; - É menos burocrático; - Para se enquadrar no Simples, é preciso ser microempreendedor individual (MEI), microempresa (ME) ou empresa de pequeno porte (EPP) e cumprir os seguintes requisitos financeiros: 1 - O MEI não pode ter faturamento anual acima de R$ 81 mil; 2 - Para ME, o teto é de R$ 360 mil por ano; 3 - O limite anual para a EPP é R$ 4,8 milhões.",
+        cnae: []
     },
     {
         id: 3,
         tiposTributacao: "Lucro Real",
-        descricao: "O enquadramento ao Lucro Real é obrigatório nos casos de bancos e corretoras de título e, ainda, caso a empresa tenha tido uma receita bruta superior a R$ 78 milhões no ano anterior. Diferentemente do Simples Nacional, os tributos são cobrados individualmente, o que requer um maior cuidado das empresas para que algum não fique em aberto. Como o cálculo de cada taxa é baseado no lucro, esse regime é indicado para empresas que não tenham uma margem de lucro muito elevada, atuem no prejuízo ou, então, tenham custos elevados em serviços ou produtos."
+        descricao: "O enquadramento ao Lucro Real é obrigatório nos casos de bancos e corretoras de título e, ainda, caso a empresa tenha tido uma receita bruta superior a R$ 78 milhões no ano anterior. Diferentemente do Simples Nacional, os tributos são cobrados individualmente, o que requer um maior cuidado das empresas para que algum não fique em aberto. Como o cálculo de cada taxa é baseado no lucro, esse regime é indicado para empresas que não tenham uma margem de lucro muito elevada, atuem no prejuízo ou, então, tenham custos elevados em serviços ou produtos.",
+        cnae: []
     },
     {
         id: 4,
         tiposTributacao: "Lucro Presumido",
-        descricao: "Neste tipo de tributação, a incidência de impostos é baseada na estimativa de lucro da empresa e calculada conforme a atividade que é desempenhada. No caso, os impostos podem variar de 8% (indústria e comércio) a 32% (serviço). Qualquer negócio que não é obrigado a aderir ao Lucro Real pode optar por esse regime, porém ele é mais vantajoso para as empresas que: - Apresentarem margem de lucro superior ao da presunção; - Tenham baixos custos operacionais; - Possuam folha de pagamento enxuta; - Não ultrapassem o faturamento de R$ 78 milhões. Existe, porém, uma desvantagem que deve ser levada em consideração. Caso a empresa lucre menos do que a média das demais empresas que exercem a mesma atividade, ela deverá pagar um imposto em cima da alíquota pré-fixada (8%)."
+        descricao: "Neste tipo de tributação, a incidência de impostos é baseada na estimativa de lucro da empresa e calculada conforme a atividade que é desempenhada. No caso, os impostos podem variar de 8% (indústria e comércio) a 32% (serviço). Qualquer negócio que não é obrigado a aderir ao Lucro Real pode optar por esse regime, porém ele é mais vantajoso para as empresas que: - Apresentarem margem de lucro superior ao da presunção; - Tenham baixos custos operacionais; - Possuam folha de pagamento enxuta; - Não ultrapassem o faturamento de R$ 78 milhões. Existe, porém, uma desvantagem que deve ser levada em consideração. Caso a empresa lucre menos do que a média das demais empresas que exercem a mesma atividade, ela deverá pagar um imposto em cima da alíquota pré-fixada (8%).",
+        cnae: []
     }
 ])
-
-use('strayCode')
-db.tributacao.find(
-    {tiposTributacao: /lucro real/i},
-    {tiposTributacao:1, descricao:1, _id:0})
-
-use('strayCode')
-db.questionario.find()
-
-use('strayCode')
-db.questionario.find(
-    {id: 2},
-    {dtQuestionario:1, _id:0, cnae: {descricao:1, ramoEmpresa:1}})
 
 /*db.abrirEmpresa.insertMany([
     {
@@ -399,13 +376,40 @@ db.questionario.find(
         tributacao: []
     },
 ])
-
+*/
 db.escolherContabilidade.insertMany([
     {
         id: 1,
-        escritorioContador: "",
-        descricao: "",
-        tributacao: []
+        escritorioContador: "Geral",
+        descricao: "A escolha entre um escritório contábil presencial ou online depende, essencialmente, da complexidade e das demandas da sua empresa. Negócios de médio e grande porte se beneficiam de um atendimento presencial mais próximo, que oferece suporte em decisões estratégicas e momentos críticos. Já para microempresas e MEIs, a contabilidade online representa uma solução eficiente e econômica, capaz de atender perfeitamente suas necessidades contábeis básicas. Avalie o perfil do seu negócio e escolha o modelo que melhor se adapta às suas demandas!",
+        tributacao: [1, 2, 3, 4]
+    },
+    {
+        id: 2,
+        escritorioContador: "Online",
+        descricao: "MEIs e Microempresas: Contabilidade Online como Solução Econômica e Eficiente. Para MEIs e microempresas de baixo faturamento, a realidade é bem diferente. A gestão contábil tende a ser mais simples, envolvendo principalmente o acompanhamento de receitas e despesas, emissão de notas fiscais e o cumprimento de obrigações fiscais básicas, como o pagamento do DAS (Documento de Arrecadação do Simples Nacional). Nesses casos, a contabilidade online pode ser uma alternativa extremamente vantajosa. Aqui estão os principais motivos: - Custo-benefício: A contabilidade online oferece um serviço mais acessível, com planos geralmente mais baratos que os escritórios tradicionais, atendendo perfeitamente as necessidades básicas de um MEI ou microempresa, sem comprometer o orçamento. - Automação e Agilidade: Muitos serviços de contabilidade online utilizam plataformas automatizadas que permitem ao empresário acessar relatórios, emitir notas fiscais e realizar outras tarefas contábeis de forma rápida e prática, muitas vezes sem a necessidade de intermediação humana constante. - Flexibilidade: Um serviço online oferece flexibilidade de acesso a qualquer momento e lugar, o que facilita a vida de empreendedores que têm pouco tempo para visitas presenciais e preferem resolver questões contábeis de maneira prática e rápida. - Simplicidade: Para negócios pequenos com operações menos complexas, a contabilidade online é suficiente para garantir que todas as obrigações fiscais e tributárias sejam cumpridas, evitando multas e complicações futuras. Portanto, para MEIs e microempresas, um serviço de contabilidade online oferece economia e praticidade, sem a necessidade de um acompanhamento presencial constante, uma vez que suas demandas contábeis são simples e podem ser facilmente automatizadas.",
+        tributacao: [1, 2]
+    },
+    {
+        id: 3,
+        escolherContabilidade: "Presencial",
+        descricao: "Empresas de Médio e Grande Porte: Priorize o Atendimento Presencial. Para empresas de médio e grande porte, a contabilidade é um processo complexo e contínuo, envolvendo uma série de atividades que vão além da simples emissão de guias ou envio de declarações. É necessário lidar com planejamento tributário, gestão de folhas de pagamento, auditorias frequentes, cumprimento de normas contábeis específicas, além de gerenciar fiscalizações e ajustes constantes em um ambiente regulatório em evolução. Nesse contexto, contar com um escritório contábil presencial pode ser a escolha mais acertada. Veja por quê: - Contato Humano e Relacionamento: O contato direto com contadores experientes permite discussões mais aprofundadas, consultas frequentes e até reuniões estratégicas que ajudam na tomada de decisões financeiras importantes. Para empresas com estruturas mais complexas, esse relacionamento é fundamental. - Suporte em Situações Críticas: Em momentos delicados, como auditorias, fiscalizações ou ajustes fiscais, a presença física de contadores pode ser determinante para garantir que tudo ocorra de maneira rápida e correta. A proximidade permite uma resposta mais ágil e personalizada. - Gestão Personalizada: Um escritório presencial tende a conhecer mais profundamente as particularidades da empresa, personalizando soluções contábeis, tributárias e financeiras, o que pode contribuir para otimizar impostos e melhorar a saúde financeira do negócio. Portanto, para empresas de médio e grande porte, onde a demanda por serviços contábeis é alta e contínua, a escolha por um escritório contábil presencial oferece um atendimento mais próximo, humano e personalizado, algo que pode ser essencial para uma gestão eficiente.",
+        tributacao: [3, 4]
     }
 ])
-*/
+
+use('strayCode')
+db.tributacao.find(
+    {id: 4},
+    {tiposTributacao:1, descricao:1, _id:0, id:1})
+
+use('strayCode')
+db.questionario.find()
+
+const dataComeco = new Date("2024-01-01")
+const dataFinal = new Date("2024-06-01")
+use('strayCode')
+db.questionario.find(
+    {dtQuestionario: {$gte: dataComeco, $lte: dataFinal}},
+    {dtQuestionario:1, _id:0, cnae: {descricao:1}})
+
